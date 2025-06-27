440d69f89ffdee86e577e81888230719e05373b1
Dryden <dsh9215@g.rit.edu>
2025-06-27 11:22:05 -0400

    Create database connection

---
+++ nhl_data/backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import psycopg2
+
+app = Flask(__name__)
+CORS(app)
+
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="Penguins11$",
+    host="localhost",
+    port="5432"
+)
+
+@app.route('/api/players')
+def get_players():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM players;")
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    data = [dict(zip(columns, row)) for row in rows]
+    cur.close()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
