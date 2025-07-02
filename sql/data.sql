8d24a05bdd13153939b5e26ece2237e1f8717204
Dryden <dsh9215@g.rit.edu>
2025-07-02 13:51:42 -0400

    Update sql

--- sql/data.sql
+++ sql/data.sql
@@ -19,3 +19,53 @@ CREATE TABLE IF NOT EXISTS nhl_data.players (
     inTop100AllTime INT,
     inHHOF INT,
 )
+
+CREATE TABLE IF NOT EXISTS nhl_data.fullTeamName (
+    playerId INT REFERENCES players(playerId),
+    language_code VARCHAR(10),
+    team_name VARCHAR(100),
+    PRIMARY KEY (playerId, language_code),
+)
+
+/* Insert default: "Edmonton Oilers", fr : "Oilers d'Edmonton" **/
+
+CREATE TABLE IF NOT EXISTs nhl_data.teamCommonName (
+    playerId INT REFERENCES players(playerId),
+    language_code VARCHAR(10),
+    team_name VARCHAR(100),
+    PRIMARY KEY (playerId, language_code),
+)
+
+/* Insert default: "Oilers" **/
+
+CREATE TABLE IF NOT EXISTS nhl_data.teamPlaceNameWithPreposition (
+    playerId INT REFERENCES players(playerId),
+    language_code VARCHAR(10),
+    team_name VARCHAR(100),
+    PRIMARY KEY (playerId, language_code),
+)
+
+/* Insert default: "Edmonton", fr : "d'Edmonton" **/
+
+CREATE TABLE IF NOT EXISTS nhl_data.firstName (
+    playerId INT REFERENCES players(playerId),
+    language_code VARCHAR(10),
+    player_first_name VARCHAR(100),
+    PRIMARY KEY (playerId, language_code),
+)
+
+/* Insert default: "Connor" **/
+
+CREATE TABLE IF NOT EXISTS nhl_data.lastName (
+    playerId INT REFERENCES players(playerId),
+    language_code VARCHAR(10),
+    player_last_name VARCHAR(100),
+    PRIMARY KEY (playerId, language_code),
+)
+
+/* Insert default: "Mcdavid" **/
+
+CREATE TABLE IF NOT EXISTS nhl_data.badges (
+    playerId INT REFERENCES players(playerId),
+    
+)
